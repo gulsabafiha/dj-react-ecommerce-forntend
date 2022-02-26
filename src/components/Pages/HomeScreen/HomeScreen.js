a1137c7e3d765888155dcde89e8fcc4a0692e9c0
Gulsaba Fiha <sabafiha111@gmail.com>
2022-02-26 23:29:48 +0600

    product design and redux added

---
+++ src/components/Pages/HomeScreen/HomeScreen.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Footer from "../../SharedComp/Footer/Footer";
+import Header from "../../SharedComp/Header/Header";
+import Product from "../Product/Product";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../../../actions/productAction";
+import Loader from "../Loader";
+import Message from "../Message";
+
+const HomeScreen = () => {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { error, loading, products } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <Container className="py-5">
+        <h1>Latest Products</h1>
+        {loading ? (
+         <Loader/>
+        ) : error ? (
+          <Message variant='danger'>{error}</Message>
+        ) : (
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        )}
+      </Container>
+      <Footer />
+    </div>
+  );
+};
+
+export default HomeScreen;
